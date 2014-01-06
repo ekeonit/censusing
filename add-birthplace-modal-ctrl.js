52b5380eb58c733368128031427a96877ecf3e0a
ekeonit <joel@sitedocs.com>
2014-01-05 22:48:45 -0800

    initial commit

---
+++ add-birthplace-modal-ctrl.js
@@ -0,0 +1,29 @@
+﻿
+angular.module('censusing')
+    .controller('AddBirthplaceModalCtrl', ['$scope', '$modalInstance', '$http',
+        function ($scope, $modalInstance, $http) {
+
+            $scope.ok = function() {
+                $modalInstance.close(true);
+            };
+
+            $scope.cancel = function() {
+                $modalInstance.dismiss();
+            };
+
+            $scope.getPlaces = function(value) {
+                return $http.get('http://maps.googleapis.com/maps/api/geocode/json', {
+                    params: {
+                        address: value,
+                        sensor: false
+                    }
+                }).then(function(response) {
+                    var places = [];
+                    angular.forEach(response.data.results, function(item) {
+                        places.push(item.formatted_address + ' (' + item.geometry.location.lat + '/' + item.geometry.location.lng + ')');
+                    });
+                    return places;
+                });
+            };
+        }
+    ]);
---
+++ censusing-ctrl.js
@@ -0,0 +1,22 @@
+﻿
+angular.module('censusing')
+    .controller('CensusingCtrl', ['$scope', '$modal',
+        function ($scope, $modal) {
+
+            $scope.addBirthplace = function() {
+
+                var m = $modal.open({
+                    templateUrl: 'add-birthplace-modal.html',
+                    controller: 'AddBirthplaceModalCtrl'
+                });
+
+                m.result.then(
+                    function(birthPlace) {
+
+                    }, function() {
+
+                    });
+            };
+
+        }
+    ]);
